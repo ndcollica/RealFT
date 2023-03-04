67f058d25507ffad79b958c39c0d713c1ad6b08f
mpcahn <mpcahn@gmail.com>
2023-03-04 10:05:19 -0700

    update index.tsx

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -6,6 +6,20 @@ import Image from "next/image"
 import React from "react";
 
 const Home: NextPage = () => {
+
+  const collections = [
+    {
+      img: "/assets/rolex.jpg",
+      title: "Rolex Cosmograph Daytona",
+      description: "New and unworn. An inspection by our certified watchmakers guarantees authenticity."
+    },
+    {
+      img: "/assets/rolex.jpg",
+      title: "Rolex Cosmograph Daytona",
+      description: "New and unworn. An inspection by our certified watchmakers guarantees authenticity."
+    },
+  ]
+
   return (
     <>
       <Head>
@@ -19,27 +33,29 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">RealFT</span>
           </h1>
-        </div>
-
-        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-          <div className="md:flex">
-            <div className="md:shrink-0">              
-              <Image
-                className="h-48 w-full object-cover md:h-full md:w-48"
-                src="/assets/rolex.jpg"
-                alt="rolex image"
-                width={100}
-                height={100}
-              />
-            </div>
-            <div className="p-8">
-              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Rolex Cosmograph Daytona</div>
-              <button className="shadow bg-base-100 rounded-box w-40">View Collection</button>
-              <p className="mt-2 text-slate-500">New and unworn. An inspection by our certified watchmakers guarantees authenticity.</p>
-              <input className="placeholder-shown:border-gray-50 w-50" placeholder="Transfer Address" />
-              <button className="shadow bg-base-100 rounded-box w-32">Start Transfer</button>
-            </div>
-          </div>
+          {
+            collections.map((val, i) => {
+              <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div className="md:flex">
+                  <div className="md:shrink-0">
+                    <Image
+                      className="h-48 w-full object-cover md:h-full md:w-48"
+                      src={val.img}
+                      alt="rolex image"
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                  <div className="p-8">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{val.title}</div>
+                    <button className="shadow bg-base-100 rounded-box w-40">View Collection</button>
+                    <p className="mt-2 text-slate-500">{val.description}</p>
+                    <input className="placeholder-shown:border-gray-50 w-50" placeholder="Transfer Address" />
+                    <button className="shadow bg-base-100 rounded-box w-32">Start Transfer</button>
+                  </div>
+                </div>
+              </div>
+            })}
         </div>
       </div>
     </>
