46e78bc21c8881e947c6a77659fa776c96db728e
mpcahn <mpcahn@gmail.com>
2023-03-03 22:05:10 -0700

    update home with start

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -84,8 +84,7 @@ export default function Header() {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </Link>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-eth</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">RealFT</span>            
           </div>
         </div>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
