36fa34bec362c2da93ee928da3bdba5c0559db72
mpcahn <mpcahn@gmail.com>
2023-03-04 09:34:53 -0700

    update index.tsx

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -18,26 +18,24 @@ const Home: NextPage = () => {
           <h1 className="text-center mb-8">
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">RealFT</span>
-          </h1>          
+          </h1>
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">            
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <Image 
-                src="/assets/rolex.jpg" 
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+          <div className="md:flex">
+            <div className="md:shrink-0">              
+              <Image
+                className="h-48 w-full object-cover md:h-full md:w-48"
+                src="/assets/rolex.jpg"
                 alt="rolex image"
                 width={100}
                 height={100}
               />
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
+            </div>
+            <div className="p-8">
+              <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Rolex</div>
+              <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
+              <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
             </div>
           </div>
         </div>
