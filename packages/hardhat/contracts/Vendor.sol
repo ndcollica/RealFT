49ccc14d713ea7af5863b76871886dedc1c783a4
ndcollica <ndcollica@gmail.com>
2023-03-05 04:47:31 -0700

    Add fields and functions.

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -4,4 +4,23 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Token.sol";
 
-contract Vendor is Ownable {}
+contract Vendor is Ownable {
+  struct objectiveType {
+    string name;
+    uint256 tokenAmount;
+  }
+  mapping(string => objectiveType) objectiveTypes;
+  string[] private objectiveType_result;
+
+  function addObjectiveType(string memory name, uint256 tokenAmount) public {
+    objectiveType memory ot = objectiveTypes[name];
+    ot.name = name;
+    ot.tokenAmount = tokenAmount;
+    objectiveType_result.push(name);
+  }
+
+  function updateType(string memory name, uint256 tokenAmount, string memory newName) public {
+    delete objectiveTypes[name];
+    addObjectiveType(newName, tokenAmount);
+  }
+}
